b1750f143eae26202034cbb1786e96c7854b6dc4
Kristiyan Kiryakov <emineo@abv.bg>
2023-12-10 17:01:59 +0200

    add: send otp func

--- api/service.go
+++ api/service.go
@@ -1,2 +1,26 @@
 package api
 
+import (
+	"errors"
+
+	"github.com/twilio/twilio-go"
+	twilioApi "github.com/twilio/twilio-go/rest/verify/v2"
+)
+
+var client *twilio.RestClient = twilio.NewRestClientWithParams(twilio.ClientParams{
+	Username: envACCOUNTSID(),
+	Password: envAUTHTOKEN(),
+})
+
+func (app *Config) twilioSendOTP(phoneNumber string) (string, error){
+	params := &twilioApi.CreateVerificationParams{}
+	params.SetTo(phoneNumber)
+	params.SetChannel("sms")
+	resp, err := client.VerifyV2.CreateVerification(envSERVICESID(), params)
+	if err !=nil{
+		return "", err
+	}
+
+	return *resp.Sid, nil
+}
+
